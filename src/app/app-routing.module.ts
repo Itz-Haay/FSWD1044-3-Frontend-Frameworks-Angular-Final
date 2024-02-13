9507e2f5a0192d040dd336d2b940cdb045f8c855
David Hay <yomanhay@comcast.net>
2024-02-13 11:35:30 -0500

    Added Components and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'products/:id', component: ProductDetailsComponent},
+  { path: 'add', component: AddProductComponent},
+  { path: 'edit/:id', component: EditProductComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -1,2 +1,9 @@
 export class Card {
+    id: number = 0;
+    name: string = '';
+    cmc: number = 0;
+    imageUrl: string = '';
+    price: number = 0;
+    flavorText: string = '';
+    rarity: string = '';
 }
