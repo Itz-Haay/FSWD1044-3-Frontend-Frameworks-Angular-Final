a4de9c181f77ded5397b43a9c718525984f1af3e
David Hay <yomanhay@comcast.net>
2024-02-13 16:15:38 -0500

    Added product list

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from '../../models/card';
+import { CardService } from '../../services/card.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
-  featuredItems: Card[] =[];
+export class HomeComponent implements OnInit {
+  featuredItems: Card[] = [];
 
-  constructor() { }
-  
-  
+  constructor(private cardService: CardService) { }
+
+
   ngOnInit(): void {
-    
+    this.getFeaturedItems();
   }
+
+  getFeaturedItems() {
+    this.cardService.getAllCards().subscribe(listCards => {
+      const randomCards = this.pickRandomCards(listCards.length, 3);
+      this.featuredItems = randomCards.map(index => listCards[index])
+    });
+  }
+
+  pickRandomCards(arraylength: number, count: number): number[] {
+    const indexes: any[] = [];
+    while(indexes.length < count) {
+      const randIndex = Math.floor(Math.random()* arraylength);
+      if(!indexes.includes(randIndex)) {
+        indexes.push(randIndex);
+      }
+    }
+    return indexes;
+  }
+
+  
+
+
+
+    
+
 
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from '../../models/card';
+import { CardService } from '../../services/card.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  cardList: Card[] = [];
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit(): void {
+    this.cardService.getAllCards().subscribe(allCards => {
+      this.cardList = allCards
+    })
+    
+  }
 }
